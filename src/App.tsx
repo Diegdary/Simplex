18f0e77467dbf38566da70efd19f594922c9a194
Diegdary <diegojose3019@gmail.com>
2025-05-11 22:23:51 -0500

    main objective function controller added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,33 @@ import "./extra_styles/index.css"
 
 function App() {
   const [objective, setObjective] = useState("Min")
+  const [func, setFunc] = useState<number[]>([])
+
+  const funcController = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    setFunc((last=>{
+      const limit = parseInt(e.target.value);
+      let t_list:number[] = [];
+      for (let i = 0; i < limit; i++) {
+        if (last.length>i) {
+          t_list.push(last[i]);
+        }
+        else{
+          t_list.push(1)
+        }
+      }
+      return t_list;
+    }));
+    
+  }
+
+  const funcValueController = (e: React.ChangeEvent<HTMLInputElement>,index:number)=>{
+    setFunc((last)=>{
+      let new_list = [...last] 
+      new_list[index]= parseInt(e.target.value)
+      console.log(new_list)
+      return new_list;
+    })
+  }
 
   return (
     <>
@@ -17,9 +44,14 @@ function App() {
         </select>)
         </p>
         <p>
-          Número de Variables: <input type="number" name="" id="nvariables" />
+          Número de Variables: <input type="number" id="nvariables" onChange={(e)=>{funcController(e)}} />
         </p>
-        <p id='funcionObjetivo'> </p>
+        <div id='funcionObjetivo'>
+          {func.map((value,index)=>(
+              <p key={index}>
+              <input type="number" id="" value={func[index]} onChange={e => funcValueController(e,index)}/> X<sub>{index+1}</sub>
+              </p>))}
+          </div>
         <h2>
           Sujeto A:
         </h2>
